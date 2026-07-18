94afc8f86e651e8d8e8b3cdab0043430a89f0549
Bhargav <bhargavpatel4319@gmail.com>
2026-07-18 15:42:17 -0400

    added delete option and customer section for skills

---
+++ src/app/shared/delete-confirm-dialog/delete-confirm-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confirm-dialog',
+  imports: [],
+  templateUrl: './delete-confirm-dialog.html',
+  styleUrl: './delete-confirm-dialog.scss',
+})
+export class DeleteConfirmDialog {}
